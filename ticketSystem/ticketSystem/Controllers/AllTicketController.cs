1330613d4d44b97201795e40a4b7f51034566770
umapornPoy <p.tunlang7@gmail.com>
2016-10-17 17:57:02 +0700

    update new page

---
+++ ticketSystem/ticketSystem/Controllers/AllTicketController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ticketSystem.Controllers
+{
+    public class AllTicketController : Controller
+    {
+        // GET: AllTicket
+        public ActionResult allTicket()
+        {
+            return View();
+        }
+    }
+}
